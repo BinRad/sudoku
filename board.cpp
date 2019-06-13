d9e80cf8294690c2d54b60d709e4e86c39f4ebe1
BinRad <bnrbradensky@gmail.com>
2019-06-13 06:30:09 -0400

    Board is working!!!

--- board.cpp
+++ board.cpp
@@ -7,37 +7,44 @@ namespace sudoku_one{
 	// matrix that holds restricted data
 //	int ini[9][9]
               //matrix that holds general data
-        int matrix[9][9];
-        for(int i = 0; i<=8; i++){
-            for (int j = 0; j <= 8; j++) {
+        for(int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++) {
                  init[i][j] = 0;
                  matrix[i][j] = 0;
             }
         }
     }
-    board::~board() {//inline function
-        delete[] init;
-        delete[] matrix;
+    board::~board() {//inline function}
     }
     // Modification functions
-    void board::mod_ini(int x, int y, int &entry) {
+    void board::mod_ini(int x, int y, int entry) {
                 matrix[x][y] = entry;
+                init[x][y] = entry;
     }
-    void board::mod_perm(int x, int y, int &entry) {
+    void board::mod(int x, int y, int entry) {
         matrix[x][y] = entry;
     }
-    void board::mod_temp(int x, int y, int &entry, bool green) {
+    void board::mod_temp(int x, int y, int entry, bool green) {
         int tempx = x;
         int tempy = y;
         if (green) {
             matrix[x][y] = entry;
         }
     }
-    void board::printout() {
-        for(int i = 0; i<=8; i++){
-            std::cout << "/n";
-            for(int j = 0; j <= 8; j++){
-                std::cout << matrix[i][j];
-            }  }
+    void const board::printout() {
+      for(int i = 0; i < 9; i++){
+          std::cout << std::endl;
+          for(int j = 0; j < 9; j++){
+              std::cout << init[i][j] << " ";
+          }
+        }
+        std::cout << std::endl;
+        for(int i = 0; i < 9; i++){
+            std::cout << std::endl;
+            for(int j = 0; j < 9; j++){
+                std::cout << matrix[i][j] << " ";
+            }
+          }
+        std::cout << std::endl;
     }
 }
--- one.cpp
+++ one.cpp
@@ -7,24 +7,20 @@
 
 int main(){
     sudoku_one::board lev;
-    for(int i = 0; i<9; i++){
+    for(int i = 0; i < 9; i++){
       std::cout << "start line: " << i + 1 << "\n";
         for(int j = 0; j < 9; j++){
             int q = 0;
             std::cin >> q;
-            if(q == 10 && j == 0){
-              for(int r = 0; r < 9; r++){
-              q = 0;
-              lev.mod_ini(i, j+r, q);
-            }
-              j = 0;
-              i++;
-            }
-            else{
-              lev.mod_ini(i , j, q);
-            }
+            lev.mod_ini(i , j, q);
         }
       std::cout << "end line: " << i + 1 << "\n";
+      if( i > 8){
+        std::cout << "all done here, good luck \n";
+
+      }
     }
+    std::cout << "Finished the for loop \n";
     lev.printout();
+    return 0;
 }
